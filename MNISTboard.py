15a41d368ff2148035a9966c2617345bf5dffd6a
EN10 <sfmneo@gmail.com>
2016-10-03 19:35:38 +0100

    TensorBoard

--- MNISTboard.py
+++ MNISTboard.py
@@ -8,17 +8,15 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder(tf.float32, [None, 10])
 
-tf.histogram_summary("weights", W)
-
-with tf.name_scope("cross_entropy"):
-  cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-tf.scalar_summary("cross_entropy", cross_entropy)
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
+tf.histogram_summary("weights", W)
+tf.scalar_summary("cross_entropy", cross_entropy)
 writer = tf.train.SummaryWriter("/tmp/mnist_logs",  sess.graph)
 merged = tf.merge_all_summaries()
 
