ce8b15defd94419163d577d2111e21acc86b3545
Enio <sfmneo@gmail.com>
2017-04-27 01:54:15 +0000

    update plt

--- MNISTplt.py
+++ MNISTplt.py
@@ -1,25 +1,34 @@
-import numpy as np
-import matplotlib.pyplot as plt
-%matplotlib inline
-import tensorflow as tf
-learn = tf.contrib.learn
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("../MNIST_data/", one_hot=True)
 
-mnist = learn.datasets.load_dataset('mnist')
-data = mnist.train.images
-labels = np.asarray(mnist.train.labels, dtype=np.int32)
-test_data = mnist.test.images
-test_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+%matplotlib inline
 
 def display(i):
-    img = test_data[i]
-    plt.title('Example %d. Label: %d' % (i, test_labels[i]))
-    plt.imshow(img.reshape((28,28)), cmap=plt.cm.gray_r) 
+    plt.imshow(mnist.train.images[i].reshape((28,28)), cmap = cm.Greys)
+    plt.title(mnist.train.labels[i])
+    plt.show()
     
 display(0)
 
-feature_columns = learn.infer_real_valued_columns_from_input(data)
-classifier = learn.LinearClassifier(feature_columns=feature_columns, n_classes=10)
-classifier.fit(data, labels, batch_size=100, steps=1000)
+import tensorflow as tf
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 10])
 
-classifier.evaluate(test_data, test_labels)
-print classifier.evaluate(test_data, test_labels)["accuracy"]
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+for _ in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
