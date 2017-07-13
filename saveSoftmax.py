7b9f485684c8c4f952b7e5f0b5f89c0745210b5d
Enio Fernandes <ef@cic.ac>
2017-07-13 21:02:32 +0000

    updated cross_entropy

--- saveSoftmax.py
+++ saveSoftmax.py
@@ -8,7 +8,8 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder(tf.float32, [None, 10])
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(
+  tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess = tf.InteractiveSession()
