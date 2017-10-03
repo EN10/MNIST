d10f65cce6abc728c2f5fde5e8e6f37ebe6c7d5f
Enio Fernandes <ef@cic.ac>
2017-10-03 14:15:05 +0000

    shape

--- imsave.py
+++ imsave.py
@@ -1,8 +1,9 @@
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("./MNIST_data/", one_hot=True)
 
-x = mnist.train.images
-x = x[1].reshape([28, 28]);
+x = mnist.train.images      # (55000, 784)
+x = x[1]                    # (784,)
+x = x.reshape([28, 28])     # (28, 28)
 
 import scipy.misc
 scipy.misc.imsave('digit.jpg', x)
